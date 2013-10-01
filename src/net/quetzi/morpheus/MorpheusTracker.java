4c7acb4690dee8f6f9c177a37c6e48afcc7b01c2
quetzi <q@quetzi.net>
2013-10-01 15:41:15 +0100

    Cleanup, bugfixes etc

--- src/net/quetzi/morpheus/MorpheusTracker.java
+++ src/net/quetzi/morpheus/MorpheusTracker.java
@@ -11,27 +11,23 @@ public class MorpheusTracker implements IPlayerTracker {
 	// IPlayerTracker implementation
 	@Override
 	public void onPlayerLogin(EntityPlayer player) {
-		Morpheus.playerSleepStatus.get(player.dimension).setPlayerAwake(
-				player.username);
+		Morpheus.playerSleepStatus.get(player.dimension).setPlayerAwake(player.username);
 	}
 
 	@Override
 	public void onPlayerLogout(EntityPlayer player) {
-		Morpheus.playerSleepStatus.get(player.dimension).removePlayer(
-				player.username);
+		Morpheus.playerSleepStatus.get(player.dimension).removePlayer(player.username);
 	}
 
 	@Override
 	public void onPlayerChangedDimension(EntityPlayer player) {
 		// Remove player from all world states
-		Iterator<Entry<Integer, WorldSleepState>> entry = Morpheus.playerSleepStatus
-				.entrySet().iterator();
+		Iterator<Entry<Integer, WorldSleepState>> entry = Morpheus.playerSleepStatus.entrySet().iterator();
 		while (entry.hasNext()) {
-			Morpheus.playerSleepStatus.get(entry.next().getKey()).removePlayer(
-					player.username);
+			Morpheus.playerSleepStatus.get(entry.next().getKey()).removePlayer(player.username);
 		}
 		// Add player to new world state
-		Morpheus.playerSleepStatus.get(player.dimension).setPlayerAwake(player.username);;
+		Morpheus.playerSleepStatus.get(player.dimension).setPlayerAwake(player.username);
 	}
 
 	@Override
