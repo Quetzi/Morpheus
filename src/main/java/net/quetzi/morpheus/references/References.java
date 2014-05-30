9643508f7ce59330c16cbe7d90f777468c5eebb6
quetzi <q@quetzi.net>
2014-05-30 13:24:18 +0100

    Update build info for jenkins

--- src/main/java/net/quetzi/morpheus/references/References.java
+++ src/main/java/net/quetzi/morpheus/references/References.java
@@ -3,9 +3,11 @@ package net.quetzi.morpheus.references;
 public class References {
     public static final String MODID = "Morpheus";
     public static final String NAME = "Morpheus";
-    public static final String VERSION = "1.7.2-1.3";
-    public static final String BUILD = "50";
-
+    public static final String MAJOR = "@MAJOR@";
+    public static final String MINOR = "@MINOR";
+    public static final String BUILD = "@BUILD_NUMBER@";
+    public static final String VERSION = "1.7.2-" + MAJOR + "." + MINOR + "." + BUILD;
+    
     public static final String ALERTS_OFF = "Text alerts turned off";
     public static final String ALERTS_ON = "Text alerts turned on";
     public static final String SETSPAWN_OFF = "Setting spawn disabled";
