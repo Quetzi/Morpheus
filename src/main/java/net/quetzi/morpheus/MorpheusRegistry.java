15a57804b97b8cb0957781bdd928f543c249245f
Quetzi <q@quetzi.net>
2014-06-24 16:16:19 +0100

    more api work

--- src/main/java/net/quetzi/morpheus/MorpheusRegistry.java
+++ src/main/java/net/quetzi/morpheus/MorpheusRegistry.java
@@ -1,14 +1,13 @@
 package net.quetzi.morpheus;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 
 import net.quetzi.morpheus.api.IMorpheusAPI;
 import net.quetzi.morpheus.api.INewDayHandler;
 
 public class MorpheusRegistry implements IMorpheusAPI {
     
-    private ArrayList<INewDayHandler> registry;
+    public static ArrayList<INewDayHandler> registry;
 
     public MorpheusRegistry() {
 
@@ -16,20 +15,15 @@ public class MorpheusRegistry implements IMorpheusAPI {
     }
 
     @Override
-    public void registerHandler(INewDayHandler newDayHandler) {
+    public void registerHandler(INewDayHandler newDayHandler, int dimension) {
         
-        registry.add(newDayHandler);
+        registry.add(dimension, newDayHandler);
     }
 
     @Override
-    public void unregisterHandler(INewDayHandler newdayhandler) {
+    public void unregisterHandler(int dimension) {
         
-        Iterator ite = registry.iterator();
-        while (ite.hasNext()) {
-            if (((INewDayHandler)ite).equals(newdayhandler)) {
-                ite.remove();
-            }
-        }
+        registry.remove(dimension);
     }
 
 }
--- src/main/java/net/quetzi/morpheus/api/IMorpheusAPI.java
+++ src/main/java/net/quetzi/morpheus/api/IMorpheusAPI.java
@@ -2,6 +2,6 @@ package net.quetzi.morpheus.api;
 
 public interface IMorpheusAPI {
 
-    void registerHandler(INewDayHandler newdayhandler);
-    void unregisterHandler(INewDayHandler newdayhandler);
+    void registerHandler(INewDayHandler newdayhandler, int dimension);
+    void unregisterHandler(int dimension);
 }
