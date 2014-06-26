79399c4c7b681647db480dce3ce3ed2e5859b581
Quetzi <q@quetzi.net>
2014-06-26 20:51:41 +0100

    Update to 1.7.10

--- src/main/java/net/quetzi/morpheus/api/IMorpheusAPI.java
+++ src/main/java/net/quetzi/morpheus/api/IMorpheusAPI.java
@@ -2,6 +2,12 @@ package net.quetzi.morpheus.api;
 
 public interface IMorpheusAPI {
 
+    /**
+     * Register your INewDayHandler with MorpheusRegistry
+     *
+     * @param newdayhandler
+     * @param dimension
+     */
     void registerHandler(INewDayHandler newdayhandler, int dimension);
     void unregisterHandler(int dimension);
 }
