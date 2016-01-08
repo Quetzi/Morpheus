73b303f8f0f92ac4d6c2d212de7a2e4c2bef321d
Quetzi <q@quetzi.net>
2016-01-08 12:10:05 +0000

    Revert changes to command class

--- src/main/java/net/quetzi/morpheus/commands/CommandMorpheus.java
+++ src/main/java/net/quetzi/morpheus/commands/CommandMorpheus.java
@@ -3,6 +3,7 @@ package net.quetzi.morpheus.commands;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.command.NumberInvalidException;
+import net.minecraft.util.BlockPos;
 import net.minecraft.util.ChatComponentText;
 import net.quetzi.morpheus.Morpheus;
 import net.quetzi.morpheus.helpers.References;
@@ -55,7 +56,7 @@ public class CommandMorpheus extends CommandBase {
             }
         } else if (astring[0].equalsIgnoreCase("disable")) {
             if (astring[1] != null) {
-                int ageToDisable = parseInt(sender, astring[1]);
+                int ageToDisable = parseInt(astring[1]);
                 if (Morpheus.register.isDimRegistered(ageToDisable)) {
                     Morpheus.register.unregisterHandler(ageToDisable);
                     sender.addChatMessage(new ChatComponentText("Disabled sleep vote checks in dimension " + ageToDisable));
@@ -69,7 +70,7 @@ public class CommandMorpheus extends CommandBase {
             sender.addChatMessage(new ChatComponentText("Morpheus version: " + References.VERSION));
         } else if (astring[0].equalsIgnoreCase("percent")) {
             if (astring[1] != null) {
-                int newPercent = parseInt(sender, astring[1]);
+                int newPercent = parseInt(astring[1]);
                 if (newPercent > 0 && newPercent <= 100) {
                     Morpheus.perc = newPercent;
                     Morpheus.config.get("settings", "SleeperPerc", 50).set(newPercent);
@@ -89,7 +90,7 @@ public class CommandMorpheus extends CommandBase {
     }
 
     @Override
-    public List addTabCompletionOptions(ICommandSender icommandsender, String[] astring) {
+    public List addTabCompletionOptions(ICommandSender icommandsender, String[] astring, BlockPos pos) {
 
         return null;
     }
