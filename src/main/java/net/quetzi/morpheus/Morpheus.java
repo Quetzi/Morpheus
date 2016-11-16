665777aad247b8a4087bbf20d55c33eaba38f0a1
Quetzi <github@quetzi.net>
2016-11-16 16:01:14 +0000

    Update required Forge version Closes #23

--- src/main/java/net/quetzi/morpheus/Morpheus.java
+++ src/main/java/net/quetzi/morpheus/Morpheus.java
@@ -17,7 +17,7 @@ import org.apache.logging.log4j.Logger;
 
 import java.util.HashMap;
 
-@Mod(modid = References.MODID, name = References.NAME, version = References.VERSION, dependencies = "required-after:Forge@[12.18.1.2011,);", acceptableRemoteVersions = "*")
+@Mod(modid = References.MODID, name = References.NAME, version = References.VERSION, dependencies = "required-after:Forge@[13.19.0.2130,);", acceptableRemoteVersions = "*")
 public class Morpheus
 {
     public static int    perc;
