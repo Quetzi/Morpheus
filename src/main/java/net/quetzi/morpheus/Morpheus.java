37460acb335bf132163c8d74ccf647ff995daebb
Quetzi <q@quetzi.net>
2016-11-17 11:27:31 +0000

    Fix forge modid error

--- src/main/java/net/quetzi/morpheus/Morpheus.java
+++ src/main/java/net/quetzi/morpheus/Morpheus.java
@@ -17,7 +17,13 @@ import org.apache.logging.log4j.Logger;
 
 import java.util.HashMap;
 
-@Mod(modid = References.MODID, name = References.NAME, version = References.VERSION, dependencies = "required-after:Forge@[13.19.0.2130,);", acceptableRemoteVersions = "*")
+@Mod(modid = References.MODID,
+        name = References.NAME,
+        version = References.VERSION,
+        dependencies = "required-after:forge@[13.19.0.2130,);",
+        acceptableRemoteVersions = "*",
+        acceptedMinecraftVersions = "[1.11,1.12)"
+)
 public class Morpheus
 {
     public static int    perc;
