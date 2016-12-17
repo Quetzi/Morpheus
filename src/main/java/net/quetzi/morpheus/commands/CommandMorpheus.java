ac064f987b6939e14c9d49f6e3f0942056b61609
Phylogeny <molecularphylo@gmail.com>
2016-12-17 01:16:03 -0800

    Removed unnecessarily overridden methods that return the same values as the superclass methods.

--- src/main/java/net/quetzi/morpheus/commands/CommandMorpheus.java
+++ src/main/java/net/quetzi/morpheus/commands/CommandMorpheus.java
@@ -151,16 +151,4 @@ public class CommandMorpheus extends CommandBase
         }
         return options;
     }
-
-    @Override
-    public boolean isUsernameIndex(String[] astring, int i)
-    {
-        return false;
-    }
-
-    @Override
-    public int getRequiredPermissionLevel()
-    {
-        return 4;
-    }
 }
