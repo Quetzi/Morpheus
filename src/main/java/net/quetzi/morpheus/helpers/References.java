4f10f348ecaa98219ec36e574d026fcded0bc548
Phylogeny <molecularphylo@gmail.com>
2016-12-17 01:17:22 -0800

    Included usage information for all possible arguments, rather than only some, preventing users from ever knowing they even exist...

--- src/main/java/net/quetzi/morpheus/helpers/References.java
+++ src/main/java/net/quetzi/morpheus/helpers/References.java
@@ -13,7 +13,7 @@ public class References
     public static final String ALERTS_OFF    = "Text alerts turned off";
     public static final String ALERTS_ON     = "Text alerts turned on";
     public static final String DISABLE_USAGE = "Use: /morpheus disable <dimension number>";
-    public static final String USAGE         = "/morpheus alerts | /morpheus percent <percentage>";
+    public static final String USAGE         = "/morpheus <alerts : version> | /morpheus percent <percentage> | /morpheus disable <dimension number>";
 
     public static final String NEWYEARTEXT    = "Good morning everyone! HAPPY NEW YEAR!";
     public static final String XMASTEXT       = "Good morning everyone! HAPPY CHRISTMAS!";
