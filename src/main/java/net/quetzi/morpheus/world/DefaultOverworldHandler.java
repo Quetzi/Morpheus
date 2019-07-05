4f3de8f263ad604bff4bac71f46cb108eaf5e8b0
Quetzi <q@quetzi.net>
2019-07-05 13:09:38 +0100

    Update to 1.14.3

--- src/main/java/net/quetzi/morpheus/world/DefaultOverworldHandler.java
+++ src/main/java/net/quetzi/morpheus/world/DefaultOverworldHandler.java
@@ -1,6 +1,7 @@
 package net.quetzi.morpheus.world;
 
 import net.minecraft.world.World;
+import net.minecraft.world.dimension.Dimension;
 import net.minecraft.world.dimension.DimensionType;
 import net.minecraftforge.fml.server.ServerLifecycleHooks;
 import net.quetzi.morpheus.api.INewDayHandler;
@@ -12,7 +13,7 @@ public class DefaultOverworldHandler implements INewDayHandler {
 
     @Override
     public void startNewDay() {
-        World world = ServerLifecycleHooks.getCurrentServer().getWorld(DimensionType.OVERWORLD);
+        World world = ServerLifecycleHooks.getCurrentServer().getWorld(DimensionType.field_223227_a_);
         world.setGameTime(world.getGameTime() + getTimeToSunrise(world));
     }
 
