c658ebfb1fbc7e2da81c8b37b537ded508005187
Quetzi <q@quetzi.net>
2019-08-26 14:18:13 +0100

    Think this should fix command issues, pesky parenthesis

--- src/main/java/net/quetzi/morpheus/commands/CommandMorpheus.java
+++ src/main/java/net/quetzi/morpheus/commands/CommandMorpheus.java
@@ -20,42 +20,52 @@ public class CommandMorpheus {
             command.getSource().sendFeedback(new StringTextComponent("Usage: " + References.USAGE).setStyle(new Style().setColor(TextFormatting.RED)), true);
             return 1;
         });
-        morpheusCommand.then(Commands.literal("version").executes((command) -> {
-            command.getSource().sendFeedback(new StringTextComponent("Morpheus version: " + References.VERSION), true);
-            return 1;
-        }));
-        morpheusCommand.then(Commands.literal("alert").executes((command) -> {
-            if (Morpheus.isAlertEnabled()) {
-                Morpheus.setAlertPlayers(false);
-                command.getSource().sendFeedback(new StringTextComponent(References.ALERTS_OFF), true);
-            } else {
-                Morpheus.setAlertPlayers(true);
-                command.getSource().sendFeedback(new StringTextComponent(References.ALERTS_ON), true);
-            }
-            return 1;
-        }));
-        morpheusCommand.then(Commands.literal("percent")).then(Commands.argument("value", IntegerArgumentType.integer(0,100)).executes((command) -> {
-            if (command.getSource().hasPermissionLevel(2)) {
-                int newPercent = IntegerArgumentType.getInteger(command, "value");
-                Morpheus.perc = newPercent;
-                Config.SERVER.perc.set(newPercent);
-                Config.SERVER.perc.save();
-                command.getSource().sendFeedback(new StringTextComponent("Sleep vote percentage set to " + Morpheus.perc + "%"), true);
-            }
-            return 1;
-        }));
-        morpheusCommand.then(Commands.literal("disable")).then(Commands.argument("dim", IntegerArgumentType.integer())).executes((command) -> {
-            if (command.getSource().hasPermissionLevel(2)) {
-                int ageToDisable = command.getArgument("dim", Integer.class);
-                if (Morpheus.register.isDimRegistered(ageToDisable)) {
-                    Morpheus.register.unregisterHandler(ageToDisable);
-                    command.getSource().sendFeedback(new StringTextComponent("Disabled sleep vote checks in dimension " + ageToDisable), true);
-                } else {
-                    command.getSource().sendFeedback(new StringTextComponent("Sleep vote checks are already disabled in dimension " + ageToDisable), true);
-                }
-            }
-            return 1;
-        });
+        morpheusCommand
+                .then(Commands.literal("version")
+                .executes((command) -> {
+                    command.getSource().sendFeedback(new StringTextComponent("Morpheus version: " + References.VERSION), true);
+                    return 1;
+                }));
+        morpheusCommand
+                .then(Commands.literal("alert")
+                .executes((command) -> {
+                    if (Morpheus.isAlertEnabled()) {
+                        Morpheus.setAlertPlayers(false);
+                        command.getSource().sendFeedback(new StringTextComponent(References.ALERTS_OFF), true);
+                    } else {
+                        Morpheus.setAlertPlayers(true);
+                        command.getSource().sendFeedback(new StringTextComponent(References.ALERTS_ON), true);
+                    }
+                    return 1;
+                }));
+        morpheusCommand
+                .then(Commands.literal("percent")
+                .then(Commands.argument("value", IntegerArgumentType.integer(0,100))
+                .executes((command) -> {
+                    if (command.getSource().hasPermissionLevel(2)) {
+                        int newPercent = IntegerArgumentType.getInteger(command, "value");
+                        Morpheus.perc = newPercent;
+                        Config.SERVER.perc.set(newPercent);
+                        Config.SERVER.perc.save();
+                        command.getSource().sendFeedback(new StringTextComponent("Sleep vote percentage set to " + Morpheus.perc + "%"), true);
+                    }
+                    return 1;
+                })));
+        morpheusCommand
+                .then(Commands.literal("disable")
+                .then(Commands.argument("dim", IntegerArgumentType.integer())
+                .executes((command) -> {
+                    if (command.getSource().hasPermissionLevel(2)) {
+                        int ageToDisable = command.getArgument("dim", Integer.class);
+                        if (Morpheus.register.isDimRegistered(ageToDisable)) {
+                            Morpheus.register.unregisterHandler(ageToDisable);
+                            command.getSource().sendFeedback(new StringTextComponent("Disabled sleep vote checks in dimension " + ageToDisable), true);
+                        } else {
+                            command.getSource().sendFeedback(new StringTextComponent("Sleep vote checks are already disabled in dimension " + ageToDisable), true);
+                        }
+                    }
+                    return 1;
+                })));
         cmdDisp.register(morpheusCommand);
     }
 }
